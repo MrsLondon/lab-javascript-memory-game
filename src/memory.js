b05163c960abc3ac6d799d5eb72c2de95ea2e5b8
ayat <abuhajayat@gmail.com>
2024-11-16 13:13:00 +0100

    class work done

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,40 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    let j, temp;
+
+    for (let i = this.cards.length - 1; i > 0; i--){
+      j = Math.floor(Math.random()*(i+1));
+      temp = this.cards[j];
+      this.cards[j] = this.cards[i];
+      this.cards[i] = temp;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    const card1Name = card1.getAttribute('data-card-name');
+    const card2Name = card2.getAttribute('data-card-name');
+
+    this.pairsClicked++;
+    document.querySelector('#pairs-clicked').innerHTML = this.pairsClicked;
+    if(card1Name === card2Name){
+      this.pairsGuessed++;
+      document.querySelector('#pairs-guessed').innerHTML = this.pairsGuessed;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    // if we have 12 pairs
+    if(this.pairsGuessed === 12){
+      console.log('Game is finished :)')
+    }
   }
 }
